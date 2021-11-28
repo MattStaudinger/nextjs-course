73fb0455fd01b17a127ead01762806d24ea10609
Matthias Staudinger <matthias.staudinger@uberall.com>
2021-11-28 17:50:08 +0100

    initial nextjs app

---
+++ nextjs-course/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next js course</title>
+        <meta name="keywords" content="web development, nextjs" />
+      </Head>
+
+      <h1>Welcome to next</h1>
+    </div>
+  );
+}
